0efcd5d19d26a82d02ee134e79d738eda7ce1c72
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-01-27 14:46:27 +0800

    build: disable "removeAttributeQuotes" option

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
       CSS: false,
       HTML: {
         'html-minifier-terser': {
+          removeAttributeQuotes: false,
           sortAttributes: false,
           sortClassName: false,
         },
